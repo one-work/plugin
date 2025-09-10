271633dd8a958142c84820f9583b1c6c2319a633
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-10 15:38:24 +0800

    expand icons

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import BluetoothPrinter from './utils/bluetooth_printer'
 import BluetoothWeigher from './utils/bluetooth_weigher'
 import PrintCPCL from './utils/print_cpcl'
 import PrintPOS from './utils/print_pos'
+import iconv from 'iconv-lite'
 
 module.exports = {
   BluetoothPrinter: BluetoothPrinter,
   BluetoothWeigher: BluetoothWeigher,
   PrintCPCL: PrintCPCL,
-  PrintPOS: PrintPOS
+  PrintPOS: PrintPOS,
+  iconv: iconv
 }
