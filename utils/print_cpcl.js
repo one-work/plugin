1d1b1b3b3244d885500b6cc5ba9e3b06d06b477d
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-18 17:30:35 +0800

    add widht logic

--- utils/print_cpcl.js
+++ utils/print_cpcl.js
@@ -65,8 +65,8 @@ export default class PrintCPCL {
     this.qrcodes.push(qrData)
   }
 
-  lineX({ x0 = 0, x1 = 40 * 8, height = 36 } = {}) {
-    this.texts.push(`L ${x0} ${this.currentY} ${x1} ${this.currentY}`)
+  lineX({ x0 = 0, x1 = 40 * 8, width = 2, height = 36 } = {}) {
+    this.texts.push(`L ${x0} ${this.currentY} ${x1} ${this.currentY} ${width}`)
     this.currentY = this.currentY + height
   }
 
