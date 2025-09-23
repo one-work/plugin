23262119808b971521c08e94fab2eb4ee81f5a19
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-23 19:50:25 +0800

    show show modal

--- utils/bluetooth_printer.js
+++ utils/bluetooth_printer.js
@@ -93,6 +93,11 @@ export default class extends Bluetooth {
                     // 所有 service 的特制值已获取完毕
                     if (servicesLength === index + 1 && this.connectedDevice.deviceId) {
                       success?.({ devices: this.allDevices })
+                    } else {
+                      this.api.showModal({
+                        title: '获取合适的特征值失败',
+                        content: JSON.stringify(res)
+                      })
                     }
                   },
                   fail: res => {
