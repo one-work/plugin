249291d26dc6495c406b9be9ff6a9da47199092b
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-24 11:09:03 +0800

    ends with fb

--- utils/bluetooth_printer.js
+++ utils/bluetooth_printer.js
@@ -75,7 +75,7 @@ export default class extends Bluetooth {
           success: res => {
             const servicesLength = res.services.length
             res.services.forEach((service, index) => {
-              if (service.isPrimary) {
+              if (service.isPrimary && service.uuid.endsWith('0000-1000-8000-00805F9B34FB')) {
                 console.debug('设备 ID：', deviceId, '主服务：', service.uuid)
                 // 获取蓝牙设备服务中所有特征
                 this.api.getBLEDeviceCharacteristics({
@@ -84,7 +84,7 @@ export default class extends Bluetooth {
                   success: res => {
                     for (const characteristic of res.characteristics) {
                       console.debug('特征值', deviceId, service.uuid, characteristic.uuid, characteristic.properties)
-                      if (characteristic.properties.write && (characteristic.properties.writeNoResponse || characteristic.uuid === 'BEF8D6C9-9C21-4C9E-B632-BD58C1009F9F')) {
+                      if (characteristic.properties.write && (characteristic.properties.writeNoResponse || characteristic.uuid.endsWith('0000-1000-8000-00805F9B34FB'))) {
                         console.debug('可写入', deviceId, service.uuid, characteristic.uuid)
                         this.connectedDevice = {
                           deviceId: deviceId,
