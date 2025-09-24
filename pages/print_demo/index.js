950a2206262801053edf1d8c007d212c6b99869e
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-24 20:44:35 +0800

    updated demo

--- pages/print_demo/index.js
+++ pages/print_demo/index.js
@@ -15,34 +15,34 @@ Page({
   printCpcl() {
     const cpcl = new PrintCPCL()
     cpcl.text('const cpcl = new PrintCPCL()')
-    cpcl.text('cpcl.text_bold("创印智能")')
-    cpcl.text_bold('创印智能')
+    cpcl.text('cpcl.text_bold("蓝牙打印")')
+    cpcl.text_bold('蓝牙打印')
     cpcl.text('cpcl.lineX()')
     cpcl.lineX()
-    cpcl.text('cpcl.text("创印智能")')
-    cpcl.text('创印智能')
+    cpcl.text('cpcl.text("蓝牙打印")')
+    cpcl.text('蓝牙打印')
     cpcl.text('cpcl.qrcode_right("https://one.work")')
     cpcl.qrcode_right('https://one.work')
     const data = cpcl.render()
     console.debug(data)
 
     this.printer.getState({
-      success: res => {
-        this.printer.writeBuffer(data)
+      success: () => {
+        this.printer.writeValue(data)
       }
     })
   },
 
   printPos() {
     const pos = new PrintPOS()
-    pos.text_big('创印智能')
-    pos.text('创印智能')
+    pos.text_big('蓝牙打印 Bluetooth Printer!')
+    pos.text('蓝牙打印')
     pos.qrcode('https://one.work')
     const data = pos.render()
     console.debug('POS处理后的数据：', data)
 
     this.printer.getState({
-      success: (res) => {
+      success: () => {
         this.printer.writeValue(data)
       }
     })
