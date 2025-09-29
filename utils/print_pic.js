dc8fd4d57a4f484e2ef7f588921d4a7f258790c5
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-29 15:28:24 +0800

    print works

--- utils/print_pic.js
+++ utils/print_pic.js
@@ -6,9 +6,10 @@ export default class PrintPic {
   }
 
   // 画 canvas 并取 RGBA
-  loadImageToCanvas(src, { succsss } = {}) {
+  loadImageToCanvas(src, { success } = {}) {
     const ctx = this.api.createCanvasContext('hiddenCanvas', this.page)
     ctx.clearRect(0, 0, 9999, 9999)
+    console.debug('图片 src：', src)
 
     this.api.getImageInfo({
       src,
@@ -17,6 +18,7 @@ export default class PrintPic {
         /* 统一缩放到 384 点宽（58 mm 纸） */
         const dw = 384
         const dh = Math.round((h * dw) / w)
+        console.debug('图片信息：', info, dw, dh)
         ctx.drawImage(src, 0, 0, dw, dh)
         ctx.draw(false, () => {
           this.api.canvasGetImageData({
@@ -26,13 +28,10 @@ export default class PrintPic {
             width: dw,
             height: dh,
             success: (res) => {
-              const data = this.imgToRaster({
-                rgba: res.data,
-                w: dw,
-                h: dh
-              })
+              console.debug('canvas 数据：', res)
+              const data = this.imgToRaster(res.data, dw, dh)
               console.debug('转化后的数据:', data)
-              success?(data)
+              success?.(data)
             }
           })
         })
@@ -46,7 +45,7 @@ export default class PrintPic {
     const raster = new Uint8Array(bytesPerLine * h)
     for (let y = 0; y < h; y++) {
       for (let x = 0; x < w; x++) {
-        const gray = rgba[(y * w + x) * 4] * 0.3 + rgba[(y * w + x) * 4 + 1] * 0.59 + rgba[(y * w + x) * 4 + 2] * 0.11
+        const gray = rgba[(y * w + x) * 4] * 0.299 + rgba[(y * w + x) * 4 + 1] * 0.587 + rgba[(y * w + x) * 4 + 2] * 0.114
         if (gray < 128) {
           const byte = Math.floor(x / 8)
           const bit = 7 - (x % 8)
@@ -60,6 +59,7 @@ export default class PrintPic {
       bytesPerLine % 256, bytesPerLine / 256,
       h % 256, h / 256
     ])
+    console.debug('图片数据：', raster)
     return Uint8Array.from([...head, ...raster])
   }
 
