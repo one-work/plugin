916d36af362dd9d1ac64c9a984b6184d6e857937
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-11 15:13:07 +0800

    fix

--- components/printer/index.js
+++ components/printer/index.js
@@ -1,6 +1,14 @@
 import BluetoothPrinter from '../../utils/bluetooth_printer'
 
 Component({
+  behaviors: ['wx://component-export'],
+
+  export() {
+    return {
+      connectedDeviceId: this.data.connectedDeviceId,
+      connectedDeviceName: this.data.connectedDeviceName
+    }
+  },
 
   properties: {
     printerName: {
