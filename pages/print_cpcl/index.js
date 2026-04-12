de793a5f56daf95e1e5486e21b90953291d501be
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-12 10:37:08 +0800

    sep pos and cpcl

---
+++ pages/print_cpcl/index.js
@@ -0,0 +1,42 @@
+import BluetoothPrinter from '../../utils/bluetooth_printer'
+import PrintCPCL from '../../utils/print_cpcl'
+import PrintPOS from '../../utils/print_pos'
+
+Page({
+  data: {
+    state: '正在连接打印机...',
+  },
+
+  onLoad(options) {
+    this.printer = new BluetoothPrinter(wx, this)
+    this.setData({ 
+      objectId: this.printer.objectId,
+      printerName: options.name
+    })
+    this.printer.registeredDevices = [wx.getStorageSync('connectedDeviceName')]
+  },
+
+  printCpcl() {
+    const cpcl = new PrintCPCL()
+    cpcl.text('const cpcl = new PrintCPCL()')
+    cpcl.text('cpcl.text_bold("蓝牙打印")')
+    cpcl.text_bold('蓝牙打印')
+    cpcl.text('cpcl.lineX()')
+    cpcl.lineX()
+    cpcl.text('cpcl.text("蓝牙打印")')
+    cpcl.text('蓝牙打印')
+    cpcl.text('cpcl.qrcode_right("https://one.work")')
+    cpcl.qrcode_right('https://one.work')
+    const data = cpcl.render()
+    console.debug(data)
+
+    this.printer.getState({
+      success: (res) => {
+        if (res.printable) {
+          this.printer.writeValue(data)
+        }
+      }
+    })
+  }
+
+})
--- pages/print_pos/index.js
+++ pages/print_pos/index.js
@@ -16,29 +16,6 @@ Page({
     this.printer.registeredDevices = [wx.getStorageSync('connectedDeviceName')]
   },
 
-  printCpcl() {
-    const cpcl = new PrintCPCL()
-    cpcl.text('const cpcl = new PrintCPCL()')
-    cpcl.text('cpcl.text_bold("蓝牙打印")')
-    cpcl.text_bold('蓝牙打印')
-    cpcl.text('cpcl.lineX()')
-    cpcl.lineX()
-    cpcl.text('cpcl.text("蓝牙打印")')
-    cpcl.text('蓝牙打印')
-    cpcl.text('cpcl.qrcode_right("https://one.work")')
-    cpcl.qrcode_right('https://one.work')
-    const data = cpcl.render()
-    console.debug(data)
-
-    this.printer.getState({
-      success: (res) => {
-        if (res.printable) {
-          this.printer.writeValue(data)
-        }
-      }
-    })
-  },
-
   printPos() {
     const pos = new PrintPOS()
     pos.text_big('蓝牙打印 Bluetooth Printer!')
