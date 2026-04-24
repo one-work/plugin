bce0cc53e51d29889f797e3185d449c874fbfd6e
qinmingyuan <mingyuan0715@foxmail.com>
2026-04-24 16:18:47 +0800

    fix

--- components/name/index.js
+++ components/name/index.js
@@ -2,41 +2,52 @@ import BluetoothPrinter from '../../utils/bluetooth_printer'
 import iconv from 'iconv-lite'
 
 Component({
-  onLoad(options) {
-    console.debug('name onload', options)
-    this.setData({
-      connectedName: options.connectedName
-    })
-
-    this.printer = new BluetoothPrinter(wx, this)
-    this.printer.registeredDevices = [this.data.connectedName]
+  properties: {
+    connectedName: {
+      type: String,
+      value: ''
+    }
   },
 
-  formSubmit(e) {
-    const input = e.detail.value
-    const name = iconv.encode(input.name, 'utf-8')
-    const length = name.length + 5
+  lifetimes: {
+    attached() {
+      console.debug('name onload', options)
+      this.setData({
+        connectedName: options.connectedName
+      })
 
-    // 计算校验位
-    let xor = 0x1f ^ 0x42
-    for (const byte of name) {
-      xor ^= byte
+      this.printer = new BluetoothPrinter(wx, this)
+      this.printer.registeredDevices = [this.data.connectedName]
     }
+  },
 
-    const data = []
-    data.push(0x1f, 0x28, 0x0f)
-    data.push(...[length % 256, Math.floor(length / 256)])
-    data.push(0x1f, 0x42)
-    data.push(...name)
-    data.push(0x00, 0x00)
-    data.push(xor)
+  methods: {
+    formSubmit(e) {
+      const input = e.detail.value
+      const name = iconv.encode(input.name, 'utf-8')
+      const length = name.length + 5
 
-    this.printer.getState({
-      success: (res) => {
-        if (res.printable) {
-          this.printer.writeValue(data)
-        }
+      // 计算校验位
+      let xor = 0x1f ^ 0x42
+      for (const byte of name) {
+        xor ^= byte
       }
-    })
+
+      const data = []
+      data.push(0x1f, 0x28, 0x0f)
+      data.push(...[length % 256, Math.floor(length / 256)])
+      data.push(0x1f, 0x42)
+      data.push(...name)
+      data.push(0x00, 0x00)
+      data.push(xor)
+
+      this.printer.getState({
+        success: (res) => {
+          if (res.printable) {
+            this.printer.writeValue(data)
+          }
+        }
+      })
+    }
   }
 })
