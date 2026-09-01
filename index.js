defead706b50b76e3d09b3750545e5b87fd4e4f4
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-01 15:57:33 +0800

    moved dpendeces

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import BluetoothPrinter from './utils/bluetooth_printer'
 import BluetoothWeigher from './utils/bluetooth_weigher'
 import { PrintPOS, PrintCPCL, PrintPic } from 'xcprinter'
-import iconv from 'iconv-lite'
-import Qrcode from 'qrcode-generator'
-import { TextEncoder, TextDecoder } 'fast-text-encoding'
 
 export {
   BluetoothPrinter,
   BluetoothWeigher,
   PrintCPCL,
   PrintPOS,
-  PrintPic,
-  iconv,
-  Qrcode,
-  TextEncoder,
-  TextEncoder
+  PrintPic
 }
