33952dee8033379ea840e11d55c4f5ec570d651d
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-01 16:27:35 +0800

    text cexo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import BluetoothPrinter from './utils/bluetooth_printer'
 import BluetoothWeigher from './utils/bluetooth_weigher'
 import { PrintPOS, PrintCPCL, PrintPic } from 'xcprinter'
-import 'fast-text-encoding'
+import 'text-encoding'
 
 export {
   BluetoothPrinter,
