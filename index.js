b7982325cb54edad4f4a36c810242afd79664186
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-01 16:24:18 +0800

    moved out

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import BluetoothPrinter from './utils/bluetooth_printer'
 import BluetoothWeigher from './utils/bluetooth_weigher'
 import { PrintPOS, PrintCPCL, PrintPic } from 'xcprinter'
+import 'fast-text-encoding'
 
 export {
   BluetoothPrinter,
